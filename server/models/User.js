d9f640a86c6fc7b07642c6beadd7d93006e25c19
DakshSingla <dakshsingla465@gmail.com>
2025-08-24 13:28:59 +0530

    Initial full project commit compleated my front end

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    _id: {type: String, requirred: true},
+    _name: {type: String, required: true},
+    _email: {type: String, required: true},
+    _image: {type: String, required: true}
+})
+
+const User = mongoose.model("User", userSchema)
+
+export default User;
